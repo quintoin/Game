b2666fdffb477bb8dbcc63b774c8ef423703f796
artemlifshits <li-ar@mail.ru>
2020-02-18 17:09:32 +0300

    Last update

--- card.py
+++ card.py
@@ -4,21 +4,7 @@ ranks = ('2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 
 
 class Card(object):
 
-    card_values = {
-        'Ace': 11,
-        '2': 2,
-        '3': 3,
-        '4': 4,
-        '5': 5,
-        '6': 6,
-        '7': 7,
-        '8': 8,
-        '9': 9,
-        '10': 10,
-        'Jack': 10,
-        'Queen': 10,
-        'King': 10
-    }
+    card_values = {'Ace': 11, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'Jack': 10, 'Queen': 10, 'King': 10}
 
     def __init__(self, suit, rank):
         self.rank = rank
